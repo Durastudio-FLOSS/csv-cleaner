6e2db7b6c839cdc0a277f171c94f1f1916fb99ed
Kelley Graham <kelley@durastudio.com>
2020-04-23 15:48:53 -0700

    resolved archive and clean with SYMLINKED=YES

--- csv-cleaner.sh
+++ csv-cleaner.sh
@@ -9,17 +9,25 @@ if [[ -e $1 || $1 == "clean" || $1 == "archive" ]]; then
    # Check that log dir exists in working dir, create if not.
    if ! [ -d log ]; then
       mkdir -v log
-
    fi
 
    # Source settings.
    . "${BASH_SOURCE%/*}/config.conf"
    echo "Sourced config.conf" > log/$LOGFILENAME
 
+   if [[ $SYMLINKSRC ]] && ! [[ -e csv-cleaner ]]; then
+      ln -s "$0" csv-cleaner
+      ln -s "${BASH_SOURCE%/*}/config.conf"
+   fi
+
+   if ! [[ "$(basename "$1")" == "$1" ]]; then
+      ln -s "$1" $(basename "$1")
+   fi
+
    # Check if in versions dir.
-   if [[ "$(basename "$PWD")" == "$VERSIONS" ]]; then
-      #echo "$(basename "$PWD") - $VERSIONS" # uncomment for testing
-      echo "\n- ERROR - You are in the $VERSIONS directory, move up to your working directory.\n" 1>&2
+   if [[ "$(basename "$PWD")" == "versions" ]]; then
+      #echo "$(basename "$PWD") - versions" # uncomment for testing
+      echo "\n- ERROR - You are in the versions directory, move up to your working directory.\n" 1>&2
       exit 1
    fi
 
@@ -31,11 +39,15 @@ if [[ -e $1 || $1 == "clean" || $1 == "archive" ]]; then
 
    if [[ "$1" == "clean" ]]; then
       echo "\n- TASK - Starting cleanup. Removing files and dir(s)...\n"
-      rm -v *.orig final*
+      rm -v final*
       rm -v versions/*  
-      rm -dv $VERSIONS
+      rm -dv versions
       rm -v log/*
       rm -dv log
+      if [[ -L "csv-cleaner" ]]; then
+         rm "csv-cleaner"
+         rm "config.conf"
+      fi
       echo "\n- TASK - Cleanup complete.\n"
       exit 0
    fi
@@ -44,19 +56,19 @@ if [[ -e $1 || $1 == "clean" || $1 == "archive" ]]; then
       echo "\n- TASK - Starting archive. Copying files and dir(s)...\n"
       ARCHIVE="archive-$(date +%d%b%Y_%T)"
       mkdir -v $ARCHIVE 
-      cp -v *.orig final* $ARCHIVE
+      cp -v final* $ARCHIVE
       cp -Rv versions $ARCHIVE
       cp -Rv log $ARCHIVE
       echo "\n- TASK - Archive to $ARCHIVE complete. Run 'clean' to remove working files.\n"
       exit 0
    fi
 
-   if ! [[ -f $1.orig ]]; then
+   if ! [[ -f versions/$(basename "$1").orig ]]; then
       echo "Backing up original file $1"
-      cp $1 $1.orig
+      cp $1 versions/$(basename "$1").orig
    else
       echo "Restoring original file $1"
-      cp $1.orig $1
+      cp versions/$(basename "$1").orig $1
    fi
 
    # Insert CSV headers.
@@ -66,27 +78,29 @@ if [[ -e $1 || $1 == "clean" || $1 == "archive" ]]; then
       ' $1
    fi
    
-   # Check that $VERSIONS dir exists in working dir, create if not.
-   if ! [ -d $VERSIONS ]; then
-      mkdir -v $VERSIONS 
+   # Check that versions dir exists in working dir, create if not.
+   if ! [ -d versions ]; then
+      mkdir -v versions 
    fi
 
    # Perform basic housekeeping.
-   sed "s/posted,,//g" $1 > $VERSIONS/01_remove_posted_$1
-   sed 's/,,/,/g' $VERSIONS/01_remove_posted_$1 > $VERSIONS/02_dbl_comma_$1
-   sed "s/,--/,+/g" $VERSIONS/02_dbl_comma_$1 > $VERSIONS/03_remove--$1
-   sed "s/,-/,/g" $VERSIONS/03_remove--$1 > $VERSIONS/04_remove-_$1
-   sed "s/,+/,-/g" $VERSIONS/04_remove-_$1 > final_$1
+   sed "s/posted,,//g" $1 > versions/01_remove_posted_$(basename "$1")
+   sed 's/,,/,/g' versions/01_remove_posted_$(basename "$1") > versions/02_dbl_comma_$(basename "$1")
+   sed "s/,--/,+/g" versions/02_dbl_comma_$(basename "$1") > versions/03_remove--$(basename "$1")
+   sed "s/,-/,/g" versions/03_remove--$(basename "$1") > versions/04_remove-_$(basename "$1")
+   sed "s/,+/,-/g" versions/04_remove-_$(basename "$1") > final_$(basename "$1")
    
    # Perform final in place substitutions read from usermap.txt.
    if [ -e usermap.txt ]; then # If usermap.txt file exists locally, use it.
-      sed -i '' -f usermap.txt final_$1
+      sed -i '' -f usermap.txt final_$(basename "$1")
    fi
 
    #sed "s/,-.*/&,/g" # unused 
 
    # Uncomment if you have one .csv file.
    #csv2ofx -m usaa final_$1 final_$1.ofx
+    
+   rm -v $(basename "$1")
 
 else
 
