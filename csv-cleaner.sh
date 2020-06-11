fe9d7016c957102471ffecd5b84be05d2f5bb4f8
Kelley Graham <kelley@durastudio.com>
2020-06-11 09:08:48 -0700

    elif else nonsense

--- csv-cleaner.sh
+++ csv-cleaner.sh
@@ -85,8 +85,8 @@ if [[ -e $1 || $1 == "clean" || $1 == "archive" || -e working_copy.csv ]]; then
       if [[ $EXPORT_NUM_COLS == 2 ]]; then
          sed s/,^[0-9].[0-9]*$/,,/g cleaned_$ORIGINAL > export_$ORIGINAL # Add debits column to credits entries
          sed -i "" s/,^-[0-9].[0-9]*$/,1.2,/g export_$ORIGINAL # Add debits column to debits entries, move debits
-         sed -i s/,^-[0-9]*$/,1/g export_$ORIGINAL # Change sign of moved debits
-      elif
+         sed -i "" s/,^-[0-9]*$/,1/g export_$ORIGINAL # Change sign of moved debits
+      else
          cp cleaned_$ORIGINAL export_$ORIGINAL
       fi   
 
